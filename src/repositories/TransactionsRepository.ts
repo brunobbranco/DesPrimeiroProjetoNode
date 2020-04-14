0de23d1728a1c07b16db12027fb3cbb4e608bede
Bruno Branco <brunobbranco@gmail.com>
2020-04-14 00:08:40 -0300

    Initial commit

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,6 +1,13 @@
 import Transaction from '../models/Transaction';
+import Balance from '../models/Balance';
 
-interface Balance {
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
+interface CreateBalanceDTO {
   income: number;
   outcome: number;
   total: number;
@@ -14,15 +21,37 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.somaPor(this.transactions, 'income');
+    const outcome = this.somaPor(this.transactions, 'outcome');
+    const total = income - outcome;
+
+    const balance = new Balance({ income, outcome, total });
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  private somaPor(objArray: Transaction[], typeKey: string): number {
+    let retorno = 0;
+    return objArray.reduce((acc, objAtual) => {
+      if (objAtual.type === typeKey) {
+        retorno = acc + objAtual.value;
+      }
+
+      return retorno;
+    }, 0);
   }
 }
 
